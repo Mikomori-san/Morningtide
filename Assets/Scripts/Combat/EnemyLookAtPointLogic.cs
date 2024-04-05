a9856ac86285418af504b0bf05cb5cc63aeb95f0
Kev <kevinraffetseder@gmail.com>
2024-04-05 14:50:07 +0100

    Camera Targetting Prototype implemented

---
+++ Assets/Scripts/Combat/EnemyLookAtPointLogic.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnemyLookAtPointLogic : MonoBehaviour
+{
+    public Transform enemyLookAtPoint;
+
+    [HideInInspector]
+    public List<GameObject> enemies;
+
+    [HideInInspector]
+    public Transform currentPlayerTransform;
+
+    int i = 0;
+
+    private bool isStartOfFight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy").ToList();
+        enemies.Sort((x, y) => GetNumberFromName(x.name).CompareTo(GetNumberFromName(y.name)));
+
+        currentPlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        enemyLookAtPoint.transform.position = currentPlayerTransform.position;
+        currentPlayerTransform.LookAt(enemyLookAtPoint.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isStartOfFight)
+        {
+            if (Keyboard.current.dKey.wasPressedThisFrame)
+            {
+                i = i == enemies.Count - 1 ? 0 : i + 1;
+            }
+            else if(Keyboard.current.aKey.wasPressedThisFrame)
+            {
+                i = i == 0 ? enemies.Count - 1 : i - 1;
+            }
+
+            if(Keyboard.current.wasUpdatedThisFrame)
+            {
+                enemyLookAtPoint.transform.position = enemies.ElementAt(i).transform.position;
+                currentPlayerTransform.LookAt(enemyLookAtPoint.position);
+            }
+        }
+    }
+
+    public void SetStartFalse()
+    {
+        isStartOfFight = false;
+        enemyLookAtPoint.transform.position = enemies.ElementAt(0).transform.position;
+    }
+    private int GetNumberFromName(string name)
+    {
+        int number;
+        if (int.TryParse(name, out number))
+        {
+            return number;
+        }
+        else
+        {
+            return int.MaxValue;
+        }
+    }
+}
---
+++ Assets/Scripts/Combat/SpaceDisableText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpaceDisableText : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject combatController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            combatController.GetComponent<EnemyLookAtPointLogic>().SetStartFalse();
+            gameObject.SetActive(false);
+        }
+
+    }
+}
